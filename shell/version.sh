48208503aa00019644c85893a7e03226b38aa347
jsonya <jsonya@163.com>
2019-04-28 21:08:31 +0800

    feat: version tag

--- shell/version.sh
+++ shell/version.sh
@@ -5,6 +5,8 @@ VERSION="$(npm --no-git-tag-version version $OPTION)"
 git add ./package.json ./package-lock.json
 git commit -m "release: $VERSION"
 git push origin master
+git tag -a $VERSION -m "chore: $VERSION"
+git push origin $VERSION
 npm publish
 echo
 echo "version: $VERSION"
